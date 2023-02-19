16b8e001c2fd1408fc64af4760ee1925f1498a8b
oht333 <mainse3208@naver.com>
2023-02-19 15:16:58 +0900

    controller signuphtml changed

--- src/main/java/com/oht/second/controller/MemberController.java
+++ src/main/java/com/oht/second/controller/MemberController.java
@@ -2,6 +2,7 @@ package com.oht.second.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,5 +16,17 @@ public class MemberController {
 	@GetMapping("/auth/signin")
 	public String signinPage() {
 		return "auth/signin";
+	}
+	
+	@GetMapping("/auth/signup")
+	public String signupPage() {
+		return "auth/signup";
+	}
+	
+	//회원가입 기능
+	@PostMapping("/auth/signup")
+	public String signup() {
+		System.out.println("signup 실행됨");
+		return "/auth/signin"; // 회원가입이 완료되면 로그인페이지 이동
 	}
 }
