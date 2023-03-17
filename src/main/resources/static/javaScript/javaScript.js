200f9f45fcfe62816d35ca32f12156e40da9ffb2
oht333 <mainse3208@naver.com>
2023-03-17 18:10:27 +0900

    processing validation

--- src/main/resources/static/javaScript/javaScript.js
+++ src/main/resources/static/javaScript/javaScript.js
@@ -7,4 +7,19 @@ $(function(){
 	$("#boardtitle").click(function(){
 		location.href = '/board/' + $(this).children(".boardNo").text();
 	})
+	
+/* enroll*/
+	$('#memId').blur(function(){
+		let enrollMemId = $('#memId').val();
+		let letterExp = /^[a-z\d]{5,20}$/;
+		
+		if(!letterExp.test($enrollMemId)){
+			$('#id-feedback').val("5-20자의 영대문자 및 소문자와 숫자만 가능합니다.");
+			$('#memId').val("");
+			return false;
+		}
+		else if(letterExp.test($enrollMemId)){
+			return true;
+		}
+	})
 })
